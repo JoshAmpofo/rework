719e206dfac1d9052c06ddacd450cd0aa0f6862b
JoshAmpofo <joshampofo92@gmail.com>
2022-11-30 21:50:00 +0000

    Warm up

---
+++ basics/prelims/short_int.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * testing short int
+ */
+int main (void)
+{
+	unsigned let = 'a';
+
+	printf("%lu\n", sizeof(let));
+
+	return (0);
+}
---
+++ basics/prelims/test_g.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * testing out %g
+ */
+int main (void)
+{
+	float num = 331.79;
+
+	printf("%g\n", num); /* %g prints a more nicer version of floats (leaves out any trailing 0s) */
+
+	return (0);
+}
